2ead03cb1caa93ffb0f264091c7e86ab47ba1b6c
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-08 21:11:56 +0100

    add okay

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,71 @@
+class EmailsController < ApplicationController
+  before_action :set_email, only: %i[ show edit update destroy ]
+
+  # GET /emails or /emails.json
+  def index
+    @emails = Email.all
+  end
+
+  # GET /emails/1 or /emails/1.json
+  def show
+  end
+
+  # GET /emails/new
+  def new
+    @email = Email.new
+  end
+
+  # GET /emails/1/edit
+  def edit
+  end
+
+  def test
+  end
+
+  # POST /emails or /emails.json
+  def create
+    @email = Email.new(object: Faker::Lorem.sentence,
+      body: Faker::Lorem.paragraphs(number: 3).join("\n"))
+
+      if @email.save
+        puts "email is success"
+      else
+        puts "email is failed"
+      end
+  end
+
+  # PATCH/PUT /emails/1 or /emails/1.json
+  def update
+    respond_to do |format|
+      if @email.update(email_params)
+        format.html { redirect_to email_url(@email), notice: "Email was successfully updated." }
+        format.json { render :show, status: :ok, location: @email }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @email.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /emails/1 or /emails/1.json
+  def destroy
+    @email.destroy
+
+    respond_to do |format|
+      format.html { redirect_to emails_url, notice: "Email was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_email
+      @email = Email.find(params[:id])
+    end
+
+
+    # Only allow a list of trusted parameters through.
+    def email_params
+      params.require(:email).permit(:object, :body)
+    end
+end
